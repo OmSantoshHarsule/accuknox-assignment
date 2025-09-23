7ca4bb4ba6a40ccbcb5053f600e66ec9a1b06fcd
Om Santosh Harsule <om.harsule.11@gmail.com>
2025-09-23 17:04:34 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -20,61 +20,61 @@ cat <<EOF > $RSPFILE
 HTTP/1.1 200 OK
 Content-Type: text/html
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>DevOps Trainee Role</title>
-    <style>
-    body {
-        font-family: Arial, sans-serif;
-        background-color: #f4f7fa;
-        margin: 0;
-        padding: 0;
-    }
-    header {
-        background-color: #e53935; /* Changed from blue to red */
-        color: white;
-        padding: 20px;
-        text-align: center;
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <title>DevOps Trainee Role</title>
+#     <style>
+#     body {
+#         font-family: Arial, sans-serif;
+#         background-color: #f4f7fa;
+#         margin: 0;
+#         padding: 0;
+#     }
+#     header {
+#         background-color: #e53935; /* Changed from blue to red */
+#         color: white;
+#         padding: 20px;
+#         text-align: center;
     
-    }
-    .fortune {
-        background-color: #ffebee; /* Changed from blue to light red */
-        padding: 15px;
-        font-family: monospace;
-        white-space: pre;
-        border: 1px solid #ffccd5; /* Changed from blue to a soft red */
-        margin: 20px;
-    }
-    .section {
-        margin: 20px;
-        padding: 20px;
-        border-radius: 8px
-        ;
+#     }
+#     .fortune {
+#         background-color: #ffebee; /* Changed from blue to light red */
+#         padding: 15px;
+#         font-family: monospace;
+#         white-space: pre;
+#         border: 1px solid #ffccd5; /* Changed from blue to a soft red */
+#         margin: 20px;
+#     }
+#     .section {
+#         margin: 20px;
+#         padding: 20px;
+#         border-radius: 8px
+#         ;
 
-    }
-    h2 {
-        color: #333;
-        left-margin: 100px;
-    }
+#     }
+#     h2 {
+#         color: #333;
+#         left-margin: 100px;
+#     }
 
-</style>
+# </style>
 
-</head>
-<body>
-    <header>
-        <h1>AccuKnox Assignment</h1>
-    </header>
+# </head>
+# <body>
+#     <header>
+#         <h1>AccuKnox Assignment</h1>
+#     </header>
 
-    <div class="fortune">
-$(cowsay "$mod")
-    </div>
-        <div class="section">
-        <h2>Submission by Om Harsule</h2>
-    </div>
-</body>
-</html>
+#     <div class="fortune">
+# $(cowsay "$mod")
+#     </div>
+#         <div class="section">
+#         <h2>Submission by Om Harsule</h2>
+#     </div>
+# </body>
+# </html>
 EOF
 }
 
